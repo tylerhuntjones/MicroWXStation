8da248de85f961f148df44c6daf08fab5500795e
Tyler H. Jones <tyler@pikespeak>
2013-01-23 04:15:18 -0500

    Added SD card logging code.

--- MicroWXStation/MicroWXStation.ino
+++ MicroWXStation/MicroWXStation.ino
@@ -3,6 +3,7 @@
 #include <dht.h>
 #include <Adafruit_BMP085.h>
 #include <LiquidCrystal.h>
+#include <SD.h>
 
 //General Definitions
 #define ON HIGH
@@ -19,6 +20,7 @@ int MinHumidity = 100;
 int MaxHumidity = 0;
 double MinDewPoint = 120;
 double MaxDewPoint = 0;
+long id = 1;
 
 // DHT22 Config
 #define DHT22_PIN 7
@@ -70,6 +72,19 @@ int UINT_LCD = 0;
 static const int UTHOLD_LCD = 5;
 int UINT_DHT = 0;
 static const int UTHOLD_DHT = 5;
+int UINT_SD = 0;
+static const int UTHOLD_SD = 60;
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+ 
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+const int chipSelect = 53;   
 
 // ------------------------------------------------------------------------------
 // setup() Core Function
@@ -84,6 +99,67 @@ void setup(void)
   pinMode(BTN_MENU, INPUT);
   pinMode(BTN_SELECT, INPUT);
   pinMode(SW_UNITS, INPUT);
+  pinMode(53, OUTPUT);
+  
+    // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_QUARTER_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card is inserted?");
+    Serial.println("* Is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+   Serial.println("Wiring is correct and a card is present.");
+  }
+ 
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+ 
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+ 
+  // print the type and size of the first FAT-type volume
+  long volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+ 
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                            // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+ 
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+ 
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+  
+-t5 (
 
   // Setup 4x20 LCD
   lcd.begin(20, 4);
@@ -288,6 +364,30 @@ void loop(void)
         break;
     }
   }
+  
+  UINT_SD++;
+   if(UINT_SD > UTHOLD_SD) {
+    //Create Data string for storing to SD card
+    //We will use CSV Format  
+    String dataString = String(id) + ", " + String(T.dht_c) + ", " + String(humidity) + ", " + String(pressure) + ", " + String(dewPointFast(T.dht_c, humidity)); 
+    
+    //Open a file to write to
+    //Only one file can be open at a time
+    File logFile = SD.open("LOG.csv", FILE_WRITE);
+    if (logFile)
+    {
+      logFile.println(dataString);
+      logFile.close();
+      Serial.println(dataString);
+    }
+    else
+    {
+      Serial.println("Couldn't open log file");
+    }
+    
+    //Increment ID number
+    id++; 
+   }
   delay(50);
 }
 
