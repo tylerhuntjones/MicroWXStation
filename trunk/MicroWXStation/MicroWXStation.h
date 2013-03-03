814da84ee83f3f7447aa7504d9c28fa7d2c7d7e6
inquirewue <inquirewue@gmail.com>
2013-03-03 08:18:53 -0500

    Added license and information headers to MicroWXStation.ino and MicroWXStation.h source files.

--- trunk/MicroWXStation/MicroWXStation.h
+++ trunk/MicroWXStation/MicroWXStation.h
@@ -1,3 +1,30 @@
+/*
+ * 
+ * MicroWXStation for Arduino Mega 2560 r3 - Version 0.2.5 
+ * Copyright (C) 2013, Tyler H. Jones (me@tylerjones.me)
+ * http://tylerjones.me/
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Filename: MicroWXStation.h
+ * 
+ * Description: The main header file associated with the Arduino IDE sketch. 
+ * Contains variable declarations and vaule definitions along with other
+ * static code that does not need to be in MicroWXStation.ide where it might
+ * be changed.
+ *
+ */
+
 #ifndef MicoWXStation_H
 #define MicoWXStation_H
 
